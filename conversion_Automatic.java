e49fdca66a575e809094e3fe075e5bd414553f29
chandramohan517 <chandramohanshanmugavel2004@gmail.com>
2024-10-01 16:21:46 +0530

    Add files via upload

---
+++ conversion_Automatic.java
@@ -0,0 +1,11 @@
+class conversion_Automatic
+{
+     public static void main (String[] args) {
+      int i=100;
+      long l=i;
+      float f=i;
+      System.out.println("int value:"+ i);
+      System.out.println("long value:"+l);
+      System.out.println("Float value:"+f);
+     }    
+}
