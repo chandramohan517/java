264eed620247b7f7976e1e6ede999a3ca1e06808
chandramohan517 <chandramohanshanmugavel2004@gmail.com>
2024-10-01 16:21:29 +0530

    Add files via upload

---
+++ A7.java
@@ -0,0 +1,24 @@
+import java.util.*;
+interface Printable
+ {
+    void print();
+ }
+ interface Showable 
+ {
+     void show();
+ }
+ class A7 implements Printable,Showable
+ {
+     public void print() { 
+         System.out.println("Hello");
+ }
+ public void show()
+ {
+    System.out.println("welcome");
+ }
+   public static void main (String[] args) {
+       A7 obj=new A7();
+       obj.print();
+       obj.show();
+   }
+ }
